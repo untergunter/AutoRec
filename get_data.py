cd10ab780989eae0a16d566ac311848a578714b7
untergunter <37904337+untergunter@users.noreply.github.com>
2022-02-06 20:50:06 +0200

    download_2_data_sets() downloads 1M and 10M data sets.
    ratings_to_train_test(1,0,1,10) gets you a train loader of the 1M dataset. x train on partitions 2-9 and y train is partitions 0 and 2-9, x test is partitions 0 and 2-9 and y test is all the data.

---
+++ get_data.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import requests
+import zipfile
+import os
+import pickle
+from shutil import rmtree
+from itertools import cycle
+import torch
+import torch.utils.data as data_utils
+
+
+def download_ratings_1_10_m():
+    for data_set_size in (1, 10):
+        url = f'http://files.grouplens.org/datasets/movielens/ml-{data_set_size}m.zip'
+        zip_name = f'ml-{data_set_size}m.zip'
+
+        r = requests.get(url, allow_redirects=True)
+        open(zip_name, 'wb').write(r.content)
+
+        with zipfile.ZipFile(zip_name, "r") as zip_ref:
+            zip_ref.extractall()
+
+        os.remove(zip_name)
+
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb+') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
+
+
+def keep_ratings_add_cv_partition():
+    for folder in ('ml-1m', 'ml-10M100K'):
+        ratings = pd.read_table(
+            f'{folder}/ratings.dat',
+            header=None,
+            delimiter='::',
+            usecols=[0, 1, 2],
+            names=['user_id', 'movie_id', 'rating'])
+        ten_partitions = cycle([i for i in range(9)])  # each user appears at least twice in each partition
+        ratings.sort_values('user_id', inplace=True)
+        ratings['partition'] = [next(ten_partitions) for count in range(len(ratings))]
+        name = folder.replace('100K', '').lower()
+        save_obj(ratings, name)
+        rmtree(folder)
+
+
+def ratings_to_train_test(dataset_size,
+                          validation_partition,
+                          train_partition,
+                          batch_size):
+    assert dataset_size in {1, 10}, 'datasets are ml-1m and ml-10m, size must be 1 or 10'
+    assert validation_partition in set(i for i in range(10)), 'using 10 cross validations'
+    assert train_partition in set(i for i in range(10)), 'using 10 cross validations'
+    assert validation_partition != train_partition
+    ratings = load_obj(f'ml-{dataset_size}m.pkl')
+
+    train_x = ratings[~ratings['partition'].isin({validation_partition, train_partition})]
+    validation_x = ratings[~ratings['partition'].isin({validation_partition})]
+
+    X_train = pd.pivot_table(train_x,
+                             values='rating',
+                             index=['user_id'],
+                             columns=['movie_id'])
+    X_train.fillna(3, inplace=True)
+    # y train is also x_test
+    Y_train = pd.pivot_table(validation_x,
+                             values='rating',
+                             index=['user_id'],
+                             columns=['movie_id'])
+    Y_train.fillna(3, inplace=True)
+
+    Y_test = pd.pivot_table(ratings,
+                            values='rating',
+                            index=['user_id'],
+                            columns=['movie_id'])
+    Y_test.fillna(3, inplace=True)
+
+    X_train_tensor = torch.tensor(X_train.values.astype(np.float32))
+    Y_train_tensor = torch.tensor(Y_train.values.astype(np.float32))
+    Y_test_tensor = torch.tensor(Y_test.values.astype(np.float32))
+
+    train_tensor = data_utils.TensorDataset(X_train_tensor, Y_train_tensor)
+    test_tensor = data_utils.TensorDataset(Y_train_tensor, Y_test_tensor)
+
+    train_loader = data_utils.DataLoader(dataset=train_tensor,
+                                         batch_size=batch_size,
+                                         shuffle=True)
+    test_loader = data_utils.DataLoader(dataset=test_tensor,
+                                        batch_size=batch_size,
+                                        shuffle=False)  # for results debugging
+
+    return train_loader, test_loader
+
+
+def download_2_data_sets():
+    download_ratings_1_10_m()
+    keep_ratings_add_cv_partition()
+
+if __name__ == '__main__':
+    download_2_data_sets()
+    train, test = ratings_to_train_test(1,
+                                        0,
+                                        1,
+                                        10)
