abb9812e91f87533192a7284877c0321c3a6032a
Oded Golden <odedgolden@gmail.com>
2022-02-15 23:24:45 +0200

    Fixes for VAE.

--- models/VarAutoRec.py
+++ models/VarAutoRec.py
@@ -19,18 +19,21 @@ class VarAutoRec(AutoRecBase):
                                          activation_function_2=activation_function_2,
                                          loss=loss,
                                          λ=λ,
-                                         lt=lr)
+                                         lr=lr)
 
         self.log_var_layer = nn.Linear(hidden_size, 2)
         self.mean_layer = nn.Linear(hidden_size, 2)
+        self.bridge_layer = nn.Linear(2, hidden_size)
 
     def forward(self, x):
         out = self.encoder(x)
         encoded = self.act_1(out)
 
         encoded_mean, encoded_var = self.mean_layer(encoded), self.log_var_layer(encoded)
-        eps = torch.randn(encoded_mean.size(0), encoded_mean.size(1)).to(encoded_mean.get_device())
+        eps = torch.randn(encoded_mean.size(0), encoded_mean.size(1))
         out = encoded_mean + eps * torch.exp(encoded_var/2)
+        out = self.bridge_layer(out)
+        # print(f"out shape: {out.shape}")
 
         out = self.decoder(out)
         out = self.act_2(out)
@@ -52,3 +55,18 @@ class VarAutoRec(AutoRecBase):
 
         self.log('train_loss', loss)
         return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y, y_mask = val_batch
+        encoded, encoded_mean, encoded_var, y_hat = self.forward(x)
+
+        # set to 0 unseen by users
+        y_hat *= y_mask
+        y *= y_mask
+
+        kl_div = -0.5*(torch.sum(1 + encoded_var - encoded_mean**2 - torch.exp(encoded_var), axis=1))
+        kl_div = kl_div.mean()
+
+        loss = torch.sum(self.loss_func(y_hat, y)*y_mask)
+        loss = loss + kl_div
+        self.log('train_loss', loss)
