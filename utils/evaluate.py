02878f928be65076f4a383ea6e354e8034e4135f
untergunter <37904337+untergunter@users.noreply.github.com>
2022-02-17 14:54:55 +0200

    evaluation matrix for AE models

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import pytorch_lightning as pl
+from sklearn.metrics import ndcg_score
 
 
 def evaluate_model(model: pl.LightningModule, test_loader, K=10):
@@ -14,11 +15,20 @@ def evaluate_model(model: pl.LightningModule, test_loader, K=10):
     for X, Y, mask in test_loader:
         if type(model).__name__ == "VarAutoRec":
             _, _, _, predictions = model(X)
-            predictions = np.argsort(predictions.squeeze().detach(), axis=1)
+            pre = predictions.squeeze().detach()
+            predictions = np.argsort(pre, axis=1)
         else:
-            predictions = np.argsort(model(X).squeeze().detach(), axis=1)
+            pre = model(X).squeeze().detach()
+            predictions = np.argsort(pre, axis=1)
+        sorted_ground = np.argsort(X*mask,axis=1)
         for i in range(predictions.shape[0]):
-            (hr, ndcg, mrr) = eval_one_rating(predictions[i, :K])
+            (hr, ndcg, mrr) = eval_one_rating(predictions[i, :K],
+                                              np.where(mask[i,:]==1)[0],
+                                              sorted_ground[i,:],
+                                              pre[i,:],
+                                              Y*mask[i:],
+                                              K
+                                              )
 
             hits.append(hr)
             ndcgs.append(ndcg)
@@ -27,28 +37,26 @@ def evaluate_model(model: pl.LightningModule, test_loader, K=10):
     return hits, ndcgs, mrrs
 
 
-def eval_one_rating(predictions):
+def eval_one_rating(predictions,mask,sg,raw_pred,masked_true,k):
     # Evaluate top rank list
-    hr = get_hit_ratio(predictions)
-    ndcg = get_ndcg(predictions)
-    mrr = get_mrr(predictions)
+    hr = get_hit_ratio(predictions,mask)
+    ndcg = get_ndcg(masked_true,raw_pred,k)
+    mrr = get_mrr(predictions,sg)
     return hr, ndcg, mrr
 
 
-def get_hit_ratio(ranklist):
-    return 0 in ranklist
+def get_hit_ratio(ranklist,mask):
+    hr = sum(i for i in mask if i in ranklist)/len(mask)
+    return hr
 
 
-def get_ndcg(ranklist):
+def get_ndcg(masked_true,raw_pred,k):
+    ndcg = ndcg_score(masked_true, raw_pred,k)
+    return ndcg
+
+
+def get_mrr(ranklist,sg):
     for i in range(len(ranklist)):
-        item = ranklist[i]
-        if item == 0:
-            return math.log(2) / math.log(i+2)
-    return 0
-
-
-def get_mrr(ranklist):
-    for i in range(len(ranklist)):
-        if ranklist[i] == 0:
-            return 1/(i + 1)
+        if ranklist[i] in sg:
+            return 1/i
     return 0
